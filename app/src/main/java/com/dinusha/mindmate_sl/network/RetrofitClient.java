48c99acb9ebb5d681c799850b51127b2db2fec31
dinusha24madusanka <dinushamadusanka54@gmail.com>
2026-08-22 18:01:54 +0530

    2026.08.22 06.01 PM

--- app/src/main/java/com/dinusha/mindmate_sl/network/RetrofitClient.java
+++ app/src/main/java/com/dinusha/mindmate_sl/network/RetrofitClient.java
@@ -2,44 +2,28 @@ package com.dinusha.mindmate_sl.network;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 import okhttp3.OkHttpClient;
 import java.util.concurrent.TimeUnit;
-
 public class RetrofitClient {
-
-    private static final String BASE_URL =
-            "https://races-assets-saturn-clearance.trycloudflare.com/";
-
+    private static final String BASE_URL = "https://pine-assessment-manhattan-britannica.trycloudflare.com/";
     private static Retrofit retrofit;
-
     public static Retrofit getRetrofitInstance() {
-
         if (retrofit == null) {
-
-            OkHttpClient client =
-                    new OkHttpClient.Builder()
+            OkHttpClient client = new OkHttpClient.Builder()
                             .connectTimeout(30, TimeUnit.SECONDS)
                             .readTimeout(60, TimeUnit.SECONDS)
                             .writeTimeout(30, TimeUnit.SECONDS)
                             .callTimeout(90, TimeUnit.SECONDS)
                             .build();
-
-            retrofit =
-                    new Retrofit.Builder()
+            retrofit = new Retrofit.Builder()
                             .baseUrl(BASE_URL)
                             .client(client)
-                            .addConverterFactory(
-                                    GsonConverterFactory.create()
-                            )
+                            .addConverterFactory(GsonConverterFactory.create())
                             .build();
         }
-
         return retrofit;
     }
-
     public static ApiService getApiService() {
-        return getRetrofitInstance()
-                .create(ApiService.class);
+        return getRetrofitInstance().create(ApiService.class);
     }
 }
