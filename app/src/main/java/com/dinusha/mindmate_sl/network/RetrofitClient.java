de062e4074811ad47185124e8bb44dc9b8742b04
dinusha24madusanka <dinushamadusanka54@gmail.com>
2026-08-30 23:30:17 +0530

    2026.08.30 11.30 PM

--- app/src/main/java/com/dinusha/mindmate_sl/network/RetrofitClient.java
+++ app/src/main/java/com/dinusha/mindmate_sl/network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import okhttp3.OkHttpClient;
 import java.util.concurrent.TimeUnit;
 public class RetrofitClient {
-    private static final String BASE_URL = " https://president-depend-usually-matthew.trycloudflare.com/";
+    private static final String BASE_URL = "https://resident-tahoe-hoping-passengers.trycloudflare.com/";
     private static Retrofit retrofit;
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
